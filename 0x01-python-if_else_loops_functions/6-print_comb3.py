6a815ab7b7f2739d93755659b9f89b37e3c00d55
KB <karim.bassam.elkhateeb@gmail.com>
2024-01-03 15:26:16 +0200

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for tens_digit in range(10):
-    for ones_digit in range(tens_digit + 1, 10):
-        print("{}{}".format(tens_digit, ones_digit), end=", " if (tens_digit, ones_digit) != (8, 9) else "\n")
+for tens in range(10):
+    for ones in range(tens + 1, 10):
+        print("{}{}".format(tens, ones), end=", " if (tens, ones) != (8, 9) else "\n")
