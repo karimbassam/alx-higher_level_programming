1b0b0682f20d3ba50366338c470424695012492d
KB <karim.bassam.elkhateeb@gmail.com>
2024-01-05 00:56:21 +0200

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     from sys import argv
     result  = 0
     for i in range(len(argv) - 1):
-        result  += int(argv[i + 1])
+        result += int(argv[i + 1])
     print(result)
 
