9cc6fb743ff2a63f00456da1ea7552223351ddf3
KB <karim.bassam.elkhateeb@gmail.com>
2024-01-08 12:08:46 +0200

    6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -6,5 +6,6 @@ def print_matrix_integer(matrix=[[]]):
                 print("{:d}".format(num))
             else:
                 print("{:d}".format(num), end=" ")
+        print()
     if not matrix:
         print()
