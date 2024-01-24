6218fa3da4598970af169b084950913e3d9cbb08
KB <karim.bassam.elkhateeb@gmail.com>
2024-01-24 04:46:23 +0200

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+class Square:
+    """
+    This is the Square class documentation.
+
+    It defines a square with a private size attribute.
+    """
+
+    def __init__(self, size):
+        """
+        Initializes a new instance of the Square class.
+
+        Parameters:
+        - size (int): The size of the square.
+
+        Note: No type/value verification is performed in this implementation.
+        """
+        self.__size = size
