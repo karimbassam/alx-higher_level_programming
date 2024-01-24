3a86f13e97f846947fc53375927eedc28daf31b8
KB <karim.bassam.elkhateeb@gmail.com>
2024-01-24 04:41:49 +0200

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """Square module"""
 
-class Square:
-    """
-    This is the Square class documentation.
 
-    It defines a basic square with no attributes or methods.
-    """
+class Square:
+    """It defines a basic square with no attributes or methods."""
     pass
