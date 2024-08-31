b0e6a9f653e56f55ee2c0c34aeb083d5ce8b9c00
KB <karim.bassam.elkhateeb@gmail.com>
2024-08-31 23:43:14 +0300

    102-python.c

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,37 +1,41 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <wchar.h>
-#include <locale.h>
 #include <Python.h>
-#include <unicodeobject.h>
+#include <wchar.h>
 
 /**
- * print_python_sting - prints info about python strings
- * @p: address of pyobject struct
+ * print_python_string - prints info about Python strings.
+ * @p: address of the PyObject struct (Python object).
+ *
+ * This function prints details about a Python string, including its type,
+ * length, and value. If the object is not a valid Python string, an error
+ * message is printed.
  */
 void print_python_string(PyObject *p)
 {
-	wchar_t *w_str;
-	
-	wprintf(L"[.] string object info\n");
-	if (strcmp(p->ob_type->tp_name, "str"))
-	{
-		wprintf(L" [ERROR] Invalid String Object\n");
-		return;
-	}
-	if (PyUnicode_IS_COMPACT_ASCII(p))
-		wprintf(L" type: comapct ascii\n");
-	else
-		wprintf(L" type: compact unicode object\n");
-	wprintf(L" length: %lu\n", PyUnicode_GET_LENGTH(p));
-	
-	w_str = PyUnicode_AsWideCharString(p, NULL);
+    wchar_t *w_str;
+
+    printf("[.] string object info\n");
+
+    if (!PyUnicode_Check(p))
+    {
+        printf("  [ERROR] Invalid String Object\n");
+        return;
+    }
+
+    if (PyUnicode_IS_COMPACT_ASCII(p))
+        printf("  type: compact ascii\n");
+    else
+        printf("  type: compact unicode object\n");
+
+    printf("  length: %ld\n", PyUnicode_GetLength(p));
+
+    w_str = PyUnicode_AsWideCharString(p, NULL);
     if (w_str == NULL)
     {
-        wprintf(L"  [ERROR] Cannot convert to wide char string\n");
+        printf("  [ERROR] Cannot convert to wide char string\n");
         return;
     }
 
     wprintf(L"  value: %ls\n", w_str);
-    PyMem_Free(w_str);
+    PyMem_Free(w_str);  // Free the memory allocated by PyUnicode_AsWideCharString
 }
+
