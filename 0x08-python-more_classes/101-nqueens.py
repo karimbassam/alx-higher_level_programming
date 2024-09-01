d74915b15fe78bb6300fd91480d5a1cb61579824
KB <karim.bassam.elkhateeb@gmail.com>
2024-09-01 03:37:35 +0300

    101-nqueens.py

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -22,7 +22,7 @@ def board_deepcopy(board):
 def get_solution(board):
     """Retrun the list lists representation of solved chessboard"""
     solution = []
-    for i in range(len(board)):
+    for r in range(len(board)):
         for c in range(len(board)):
             if board[r][c] == "Q":
                 solution.append([r, c])
@@ -30,7 +30,7 @@ def get_solution(board):
     return (solution)
 
 
-def x(board, row, col):
+def xout(board, row, col):
     """
     Spots on chessboard all spots where non-attacking queens can
     no longer be played
@@ -46,7 +46,8 @@ def x(board, row, col):
     for c in range(col - 1, -1, -1):
         board[row][c] = "x"
     # x all spots below
-    for r in range(row + 1, len(board))
+    for r in range(row + 1, len(board)):
+        board[r][col] = "x"
     # x all spots above
     for r in range(row - 1, -1, -1):
         board[r][col] = "x"
@@ -80,7 +81,7 @@ def x(board, row, col):
         c -= 1
 
 
-def recursive_solve(board_row, queens, solutions):
+def recursive_solve(board, row, queens, solutions):
     """
     Recursively solve an N-queens puzzle.
     Args:
@@ -97,7 +98,7 @@ def recursive_solve(board_row, queens, solutions):
 
     for c in range(len(board)):
         if board[row][c] == " ":
-            tmp_board = board = board_deepcopy(board)
+            tmp_board = board_deepcopy(board)
             tmp_board[row][c] = "Q"
             xout(tmp_board, row, c)
             solutions = recursive_solve(tmp_board, row + 1,
@@ -110,7 +111,7 @@ if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: nqueens N")
         sys.exit(1)
-    if sys.qrgv[1].isdigit() is False:
+    if sys.argv[1].isdigit() is False:
         print("N must be a number")
         sys.exit(1)
     if int(sys.argv[1]) < 4:
