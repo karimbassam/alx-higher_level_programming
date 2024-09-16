b687b2fcea7a6f4790442be720ec046fc43e352d
KB <karim.bassam.elkhateeb@gmail.com>
2024-09-16 16:58:22 +0300

    101-locked_class.py

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+"""
+Module for LockedClass.
+This module defines a LockedClass that only allows setting
+'first_name' as an attribute.
+"""
+
 
 class LockedClass:
+    """
+    A class that prevents the dynamic creation of new attributes,
+    except for the attribute 'first_name'.
+    """
     __slots__ = ['first_name']
