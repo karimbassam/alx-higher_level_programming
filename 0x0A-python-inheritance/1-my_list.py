e20033bd63b235fb0018a1afd2e8d72cf7e113f4
KB <karim.bassam.elkhateeb@gmail.com>
2024-09-27 14:27:29 +0300

    1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -5,10 +5,10 @@
 class MyList(list):
     """A custom list class that inherits from the built-in list class.
 
-    This class has a method to print the list elements sorted in ascending order.
+    The class has a method to print the list elements sorted in ascen order
     """
 
     def print_sorted(self):
-        """Prints the list elements in ascending order without modifying the original list."""
+        """Prints the list elements in ascen order without modifying
+        the original list"""
         print(sorted(self))
-
