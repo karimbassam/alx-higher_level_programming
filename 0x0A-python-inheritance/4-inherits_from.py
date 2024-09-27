ae9ba669af1855ac4b9e0d9d5fa6054ceb48e6ba
KB <karim.bassam.elkhateeb@gmail.com>
2024-09-27 14:42:59 +0300

    3-is_kind_of_class.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,11 +4,11 @@
 
 def inherits_from(obj, a_class):
     """Check if obj is an instance of a class that inherited from a_class.
-    
+
     Args:
         obj: The object to check.
         a_class: The class to compare with.
-    
+
     Returns:
         True if obj is an instance of a class that inherited
         from a_classbut not an instance of a_class
