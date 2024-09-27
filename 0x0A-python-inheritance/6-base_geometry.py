22647b2cb4b1c0ebe16be915189d798d4d68b3ef
KB <karim.bassam.elkhateeb@gmail.com>
2024-09-27 14:53:10 +0300

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for improving BaseGeometry"""
+
+
+class BaseGeometry:
+    """A class that represents BaseGeometry."""
+
+    def area(self):
+        """Public instance method that raises an Exception."""
+        raise Exception("area() is not implemented")
